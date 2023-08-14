9204003b1f1af3f8120b1c446d6c4547137b6915
Nikolay Kolomyytsev <kolya.ya@gmail.com>
2023-08-14 21:14:23 +0200

    refaktor: small updates for main-nav.js

--- themes/headcrash/assets/js/modules/main-nav.js
+++ themes/headcrash/assets/js/modules/main-nav.js
@@ -15,32 +15,19 @@ navTogglerBtn.addEventListener('click', ({ target }) => {
         headerNav.setAttribute('data-visibility', 'visible');
         headerNav.classList.add('header__nav--open');
     } else {
-        const trEnd = (e) => {
-            console.log("END", e.propertyName, navVisibility)
-            headerNav.setAttribute('data-visibility', 'hidden');
-            headerNav.removeEventListener('transitionend', trEnd)
-        };
-
         headerNav.classList.remove('header__nav--open');
-        headerNav.addEventListener('transitionend', trEnd)
+        
+        // wait for transition to end
+        headerNav.addEventListener('transitionend', () => {
+            headerNav.setAttribute('data-visibility', 'hidden');
+        }, { once: true });
     }
 
     // avoid body scrolling
     if (document.body.style.position === 'fixed') {
-        document.body.style.position = null;
-        document.body.style.top = null;
+        document.body.style = null;
     } else {
         document.body.style.position = 'fixed';
-        document.body.style.top = `-${window.scrollY}px`;
-        document.body.style.left = '0px';
-        document.body.style.right = '0px';
+        document.body.style.inset = '0';
     }
 });
-
-// headerBottom.addEventListener('keyup', (e) => {
-//     if (e.code === 'Escape' && document.body.style.position === 'fixed') {
-//         document.body.style.position = null;
-//         document.body.style.top = null;
-//         headerNav.classList.toggle('header__nav--open');
-//     }
-// });
